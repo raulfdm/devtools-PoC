0ffe1e92f1299866cceebae82d9eb256886e9b40
Raul Melo <melo.raulf@gmail.com>
2021-07-24 18:14:01 +0200

    chore: add styles for DevTools

--- src/devtools/domain/DevTools/DevTools.tsx
+++ src/devtools/domain/DevTools/DevTools.tsx
@@ -1,13 +1,15 @@
-import { devTools } from "../devtools";
-import { GroupSection } from "../ui";
+import { devTools } from "../../devtools";
+import { GroupSection } from "../../ui";
+
+import styles from "./DevTools.module.scss";
 
 export function DevTools() {
   const groups = devTools.internalApi.getGroups();
   return (
     /* @ts-ignore */
     <Fragment>
-      <h1>DevTools</h1>
-      <div>
+      <h1 className={styles.title}>DevTools</h1>
+      <div className={styles.itemsWrapper}>
         {groups.map((group) => (
           <GroupSection group={group} key={group.label} />
         ))}
